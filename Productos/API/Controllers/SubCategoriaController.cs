dedecdd80e8a9716f781ba11b8b6f16176be9cfe
Gabz2808 <gabrielgranera28@hotmail.com>
2026-03-15 18:30:37 -0600

    Primer commit

---
+++ Productos/API/Controllers/SubCategoriaController.cs
@@ -0,0 +1,31 @@
+﻿using Abstracciones.Interfaces.Api;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using DA;
+using Flujos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class SubCategoriaController : ControllerBase, ISubCategoriaController
+
+    {
+        private readonly ISubCategoriaFlujo _subcategoriaFlujo;
+
+        public SubCategoriaController(ISubCategoriaFlujo subcategoriaFlujo)
+        {
+            _subcategoriaFlujo = subcategoriaFlujo;
+        }
+
+        [HttpGet("ObtenerSubCategorias")]
+        public async Task<IActionResult> Obtener(Guid IdCategoria)
+        {
+            var resultado = await _subcategoriaFlujo.Obtener(IdCategoria);
+            if (!resultado.Any())
+                return NoContent();
+            return Ok(resultado);
+        }
+    }
+}
---
+++ Web/Web/Web/Pages/Productos/Index.cshtml.cs
@@ -0,0 +1,34 @@
+using Abstracciones.Interfaces.Reglas;
+using Abstracciones.Modelos.Abstracciones.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace web.Pages.Productos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguracion _configuracion;
+        public IList<ProductoResponse> Productos { get; set; } = default!;
+        public IndexModel(IConfiguracion configuracion)
+        {
+            _configuracion = configuracion;
+        }
+        public async Task OnGet()
+        {
+            string endpoint = _configuracion.ObtenerMetodo("ApiEndPoints",
+                "ObtenerProductos");
+            var cliente = new HttpClient();
+            var solicitud = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            var respuesta = await cliente.SendAsync(solicitud);
+            respuesta.EnsureSuccessStatusCode();
+            var resultado = await respuesta.Content.ReadAsStringAsync();
+            var opciones = new JsonSerializerOptions
+            { PropertyNameCaseInsensitive = true };
+            Productos = JsonSerializer.Deserialize<List<ProductoResponse>>(resultado, opciones);
+
+
+        }
+    }
+}
